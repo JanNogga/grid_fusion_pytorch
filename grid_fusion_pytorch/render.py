fix bug related to scaling of depth images

grid_fusion_pytorch/render.py
@@ -108,7 +108,7 @@ def batched_grid_forward(xyz, grid, range_min, range_max, class_channels=True):
 
 # adapted from https://github.com/vsitzmann/light-field-networks/blob/master/geometry.py
 # label_masks, world_cams, cam_ks, n_rays=-1, depth=None
-def sample_rays(world_cam, cam_k, n_rays=-1, semseg=None, depth=None, H=None, W=None):
+def sample_rays(world_cam, cam_k, n_rays=-1, semseg=None, depth=None, H=None, W=None, normalize=True):
     assert semseg is not None or depth is not None or (H is not None and W is not None)
     device = world_cam.device
     # get ray origins from world_cam
@@ -151,7 +151,10 @@ def sample_rays(world_cam, cam_k, n_rays=-1, semseg=None, depth=None, H=None, W=
     # more readable version (swap if this is bad for performance)
     world_coords = (world_cam.unsqueeze(-3) @ cam_coords_hom.unsqueeze(-1))[...,:3, 0]
     # get normalized ray directions
-    ray_dirs = F.normalize(world_coords - ray_origs.unsqueeze(-2), dim=-1)
+    if normalize:
+        ray_dirs = F.normalize(world_coords - ray_origs.unsqueeze(-2), dim=-1)
+    else:
+        ray_dirs = world_coords - ray_origs.unsqueeze(-2)
     return ray_origs, ray_dirs, gt_labels, gt_depth
 
 ### get uniformly spaced points from rays

lib/counting_model.py
@@ -1,5 +1,6 @@
 import os
 
+import torch
 from torch.utils.cpp_extension import load
 
 parent_dir = os.path.dirname(os.path.abspath(__file__))
@@ -27,7 +28,9 @@ def apply_counting_model(Grids, Origs, Dirs, Dists, RangeMin, RangeMax, Semseg=N
             # modify distances for background class rays
             dists_background = Dists.clone()
             dists_background[Semseg.sum(-1) > 0] = background_range if not invalidate_background else -1.
-            return counting_model_util_cuda.counting_model_bayes_free_function(Grids, Origs, Dirs, Dists, RangeMin, RangeMax, Semseg, n_steps)
+            grid_update = counting_model_util_cuda.counting_model_bayes_free_function(Grids, Origs, Dirs, Dists, RangeMin, RangeMax, Semseg, n_steps)
+            grid_update[:,2:] -= torch.logsumexp(grid_update[:,2:], dim=1, keepdim=True)
+            return grid_update
     # if there are no class channels, apply counting model without Bayes filter
     else:
         # if there is a semantic segmentation, warn the user and apply counting model without using it